64acffd0405dd2027965f7bae814ff2d57fdaf6c
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-12-29 13:46:42 +0530

    Refactor Code

--- AutoShots.py
+++ AutoShots.py
@@ -6,9 +6,7 @@ import os
 import time
 
 
-
-class OnMyWatch:
-
+class AutoShots:
     watchDirectory = "./Sample"
 
     def __init__(self):
@@ -16,7 +14,7 @@ class OnMyWatch:
 
     def run(self):
         event_handler = Handler()
-        self.observer.schedule(event_handler, self.watchDirectory, recursive = True)
+        self.observer.schedule(event_handler, self.watchDirectory, recursive=True)
         self.observer.start()
         try:
             while True:
@@ -27,39 +25,38 @@ class OnMyWatch:
 
         self.observer.join()
 
+    def MoveFiles(event):
+        oldFile = event.src_path.split("/")[-1]
+
+        newFilePath = "/Users/pushpinderpalsingh/Documents/Learning/Other Projects/Python/test/images/"
+        newFile = datetime.datetime.today().strftime('%H%d%m%y') + "-" + oldFile
+
+        os.rename(event.src_path, newFilePath + newFile)
+        print("Renaming and Moving Successful")
+
+        AutoShots.updateGit(newFile)
+
+    def updateGit(file):
+        file = "images/" + file
+        repo = Repo("/Users/pushpinderpalsingh/Documents/Learning/Other Projects/Python/test/")
+        origin = repo.remote(name='origin')
+        origin.pull()
+        repo.index.add([file])
+        repo.index.commit("Update From Script")
+        origin.push()
+        print("Pushed latest")
+
+
 class Handler(FileSystemEventHandler):
 
     @staticmethod
     def on_created(event):
-        MoveFiles(event)
-
+        AutoShots.MoveFiles(event)
 
     @staticmethod
     def on_moved(event):
-        MoveFiles(event)
+        AutoShots.MoveFiles(event)
 
 
-def MoveFiles(event):
-    oldFile = event.src_path.split("/")[-1]
-
-    newFilePath = "/Users/pushpinderpalsingh/Documents/Learning/Other Projects/Python/test/images/"
-    newFile = datetime.datetime.today().strftime('%H%d%m%y') + "-" + oldFile
-
-    os.rename(event.src_path, newFilePath + newFile)
-    print("Renaming and Moving Successful")
-
-
-    updateGit(newFile)
-
-def updateGit(file):
-    file = "images/" + file
-    repo = Repo("/Users/pushpinderpalsingh/Documents/Learning/Other Projects/Python/test/")
-    origin = repo.remote(name='origin')
-    origin.pull()
-    repo.index.add([file])
-    repo.index.commit("Update From Script")
-    origin.push()
-    print("Pushed latest")
-
-watch = OnMyWatch()
+watch = AutoShots()
 watch.run()
